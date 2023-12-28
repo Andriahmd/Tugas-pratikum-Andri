a30565adcca23b7f6dd42fb7295a366cad3f10fc
Andriahmd <153442820+Andriahmd@users.noreply.github.com>
2023-12-28 17:24:29 +0700

    Update SimpleGraph.java

--- Graph_java/SimpleGraph.java
+++ Graph_java/SimpleGraph.java
@@ -19,12 +19,12 @@ class SimpleGraph {
         // adjacencyList.get(destination).add(source);
     }
 
-    // Mendapatkan daftar tetangga dari node
+    // code untuk Mendapatkan daftar Neighbors dari node
     public List<Integer> getNeighbors(int node) {
         return adjacencyList.getOrDefault(node, Collections.emptyList());
     }
 
-    // Mencetak graf
+    // code yg digunakan untuk Mencetak graf
     public void printGraph() {
         for (int node : adjacencyList.keySet()) {
             System.out.print("Node " + node + " terhubung dengan: ");
@@ -37,17 +37,21 @@ class SimpleGraph {
 
     public static void main(String[] args) {
         SimpleGraph graph = new SimpleGraph();
+        
+        // code untuk Menambahkan node ke graf
         graph.addNode(0);
         graph.addNode(1);
         graph.addNode(2);
         graph.addNode(3);
 
+        // code untuk Menambahkan edge atau sisi ke graf
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(1, 2);
         graph.addEdge(2, 0);
         graph.addEdge(2, 3);
 
+        // code untuk mencetak graph
         graph.printGraph();
     }
 }
