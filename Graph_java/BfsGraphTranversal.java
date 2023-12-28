6766f33377aa6c1a25182ea3b6297b6335b892cc
Andriahmd <153442820+Andriahmd@users.noreply.github.com>
2023-12-28 10:44:33 +0700

    Update BfsGraphTranversal.java

--- Graph_java/BfsGraphTranversal.java
+++ Graph_java/BfsGraphTranversal.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// code dibawah digunakan untuk mendeklasikan kelas Edges
 class Edges {
     String destination;
     int weight;
@@ -9,70 +10,74 @@ class Edges {
         this.weight = weight;
     }
 }
+
+// Sebuah code yang di gunakan untuk mendeklasikan kelas BfsGraphTraversal
 public class BfsGraphTranversal {
     private Map<String, List<Edges>> adjacencyList;
 
     public BfsGraphTranversal() {
-        adjacencyList = new HashMap<>();  
-}
- // Menambahkan node baru ke graf
- public void addNode(String node) {
-    adjacencyList.putIfAbsent(node, new ArrayList<>());
-}
+        adjacencyList = new HashMap<>();
+    }
 
-// Menambahkan edge berbobot ke graf
-public void addEdge(String source, String destination, int weight) {
-    // Pastikan node sumber ada dalam adjacency list
-    adjacencyList.putIfAbsent(source, new ArrayList<>());
+    // Menambahkan node baru ke graf
+    public void addNode(String node) {
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
 
-    // Tambahkan edge berbobot
-    adjacencyList.get(source).add(new Edges(destination, weight));
-}
+    // Menambahkan edge yang memiliki berbobot ke graf
+    public void addEdge(String source, String destination, int weight) {
+        // Pastikan node sumber ada dalam adjacency list
+        adjacencyList.putIfAbsent(source, new ArrayList<>());
 
-// Mendapatkan daftar tetangga dan bobot dari node
-public List<Edges> getNeighbors(String node) {
-    return adjacencyList.getOrDefault(node, Collections.emptyList());
-}
+        // lalu Tambahkan edge berbobot
+        adjacencyList.get(source).add(new Edges(destination, weight));
+    }
 
-// BFS traversal dari node sumber
-public void bfsTraversal(String start) {
-    Set<String> visited = new HashSet<>();
-    Queue<String> queue = new LinkedList<>();
+    // code yang digunakan untuk mendapatkan Neighbors dan bobot dari node
+    public List<Edges> getNeighbors(String node) {
+        return adjacencyList.getOrDefault(node, Collections.emptyList());
+    }
 
-    queue.add(start);
-    visited.add(start);
+    // code untuk melakukan BFS traversal dari node sumber
+    public void bfsTraversal(String start) {
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
 
-    while (!queue.isEmpty()) {
-        String current = queue.poll();
-        System.out.print(current + " ");
+        queue.add(start);
+        visited.add(start);
 
-        // Menambahkan semua tetangga yang belum dikunjungi ke antrian
-        for (Edges edge : adjacencyList.getOrDefault(current, Collections.emptyList())) {
-            if (!visited.contains(edge.destination)) {
-                queue.add(edge.destination);
-                visited.add(edge.destination);
-                //System.out.print("-> (" + edge.weight + ") " + edge.destination + " ");
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+            System.out.print(current + " ");
+
+            // Menambahkan semua Neighbors yang belum masuk ke antrian
+            for (Edges edge : adjacencyList.getOrDefault(current, Collections.emptyList())) {
+                if (!visited.contains(edge.destination)) {
+                    queue.add(edge.destination);
+                    visited.add(edge.destination);
+                    // System.out.print("-> (" + edge.weight + ") " + edge.destination + " ");
+                }
             }
         }
+        System.out.println();
     }
-    System.out.println();
-}
 
-public static void main(String[] args) {
-    BfsGraphTranversal graph = new BfsGraphTranversal();
-    graph.addNode("A");
-    graph.addNode("B");
-    graph.addNode("C");
-    graph.addNode("D");
+    public static void main(String[] args) {
+        BfsGraphTranversal graph = new BfsGraphTranversal();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addNode("D");
 
-    graph.addEdge("A", "B", 5);
-    graph.addEdge("A", "C", 3);
-    graph.addEdge("B", "C", 2);
-    graph.addEdge("C", "D", 4);
+        graph.addEdge("A", "B", 5);
+        graph.addEdge("A", "C", 3);
+        graph.addEdge("B", "C", 2);
+        graph.addEdge("C", "D", 4);
 
-    graph.bfsTraversal("A");
-}
-
-
+        graph.bfsTraversal("A");
+        // code main yang membuat objek graf, menambahkan node, menambahkan tepi, dan
+        // kemudian memanggil code bfsTraversal untuk melakukan BFS traversal dari
+        // node "A".
+    }
 
 }
